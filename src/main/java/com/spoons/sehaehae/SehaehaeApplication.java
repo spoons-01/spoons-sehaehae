e553181f93304cf5282ed603995bcffdfbfb347e
popipopipoi <somt1128@gmail.com>
2023-09-25 11:50:39 +0900

    main first commit

---
+++ src/main/java/com/spoons/sehaehae/SehaehaeApplication.java
@@ -0,0 +1,13 @@
+package com.spoons.sehaehae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SehaehaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SehaehaeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spoons/sehaehae/SehaehaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spoons.sehaehae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SehaehaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
