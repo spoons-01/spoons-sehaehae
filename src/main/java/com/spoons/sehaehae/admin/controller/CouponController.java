146c9d2fed84ac419abb381cc0ec392659a8fa5d
dhnn412 <dhnn412@gmail.com>
2023-10-06 17:50:34 +0900

    coupon list

--- src/main/java/com/spoons/sehaehae/admin/controller/CouponController.java
+++ src/main/java/com/spoons/sehaehae/admin/controller/CouponController.java
@@ -5,19 +5,27 @@ import com.spoons.sehaehae.admin.service.CouponService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/coupon")
 public class CouponController {
     private final CouponService couponService;
     public CouponController(CouponService couponService){this.couponService=couponService;}
-    @GetMapping("/coupon")
+    @GetMapping("/list")
     public String getCouponList(Model model){
 
         List<CouponDTO> couponList = couponService.selectCouponList();
         model.addAttribute("coupon", couponList);
 
-        return "/admin/coupon/coupon";
+        return "/admin/coupon/couponlist";
+    }
+    @GetMapping("/regist")
+    public String registCoupon(Model model){
+        return "/admin/coupon/couponRegist";
     }
 }
