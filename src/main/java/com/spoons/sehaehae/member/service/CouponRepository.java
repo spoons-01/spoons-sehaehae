c8cd716bd5fd99ddf1249a4f0626163cc676ad92
asoong2 <yang950454@gmail.com>
2023-10-14 12:20:34 +0900

    #36
    2023.10.14 오후 12:20
    유저 로그인 ui 수정 및 mysehae 쿠폰조회, 등급조회 업데이트

---
+++ src/main/java/com/spoons/sehaehae/member/service/CouponRepository.java
@@ -0,0 +1,36 @@
+package com.spoons.sehaehae.member.service;
+
+import com.spoons.sehaehae.member.dto.MemberDTO;
+import org.springframework.jdbc.core.JdbcTemplate;
+        import org.springframework.security.core.Authentication;
+        import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Controller
+public class CouponRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public CouponRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public int countCouponsByMemberNo(int memberNo) {
+        // 현재 로그인한 사용자의 Authentication 객체 얻기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getPrincipal() instanceof MemberDTO) {
+            // MemberDTO 객체에서 MEMBER_NO 얻기
+            MemberDTO memberDTO = (MemberDTO) authentication.getPrincipal();
+            memberNo = memberDTO.getMemberNo();
+
+            // SQL 쿼리 실행
+            String sql = "SELECT COUNT(*) FROM TBL_CPBOX WHERE MEMBER_NO = ?";
+            return jdbcTemplate.queryForObject(sql, new Object[]{memberNo}, Integer.class);
+        }
+
+        return 0; // 혹시 실패할 경우 기본값 설정
+    }
+}
+
