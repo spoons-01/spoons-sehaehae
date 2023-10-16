5cd34981bc0edc0f0e49d7439c1c894fb92847e0
asoong2 <yang950454@gmail.com>
2023-10-16 09:13:23 +0900

    #8-member-regist
    2023.10.16 오전 09:12
    회원가입 일부 수정 및 나의세해 전체 조회

--- src/main/java/com/spoons/sehaehae/member/dao/MemberMapper.java
+++ src/main/java/com/spoons/sehaehae/member/dao/MemberMapper.java
@@ -1,8 +1,10 @@
 package com.spoons.sehaehae.member.dao;
 
+import com.spoons.sehaehae.board.dto.ReviewDTO;
 import com.spoons.sehaehae.member.dto.MemberDTO;
 import com.spoons.sehaehae.member.dto.MyCouponDTO;
 import com.spoons.sehaehae.member.dto.MyOrderDTO;
+import com.spoons.sehaehae.member.dto.MyPointDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,18 +13,36 @@ import java.util.List;
 @Mapper
 public interface MemberMapper {
     MemberDTO findByMemberId(String memberId);
+
     String selectMemberById(String memberId);
+
     int insertMember(MemberDTO member);
+
     int insertMemberRole();
+
     int insertMemberLevel();
+
     int countMemberByTel(String tel);
+
     @Transactional
     int updateMember(MemberDTO modifyMember);
+
     @Transactional
     void insertThumbnailContent(MemberDTO modifyMember);
-    List<MemberDTO> selectMemberList();
+
     List<MyOrderDTO> findMyOrder(String memberId);
+
     List<MyCouponDTO> findMyCoupon(int memberNo);
+
+    int findMyPoint(int memberNo);
+
+    List<ReviewDTO> findMyReview(int memberNo);
+
+
+
+    /* 동한님 */
+
+    List<MemberDTO> selectMemberList();
 
     void adUpdateMember(MemberDTO member);
 
@@ -42,4 +62,7 @@ public interface MemberMapper {
     int getFourth();
 
     int getFifth();
+
+
+
 }
