e58040953cba5a6290005efa2b643478d854d421
popipopipoi <somt1128@gmail.com>
2023-10-20 09:09:15 +0900

    권한설정 수정(장바구니, 마이페이지설정, 후기글보기)

--- src/main/resources/static/javaScript/modalEvent.js
+++ src/main/resources/static/javaScript/modalEvent.js
@@ -1,10 +1,10 @@
 /* 모달 이벤트 */
 window.addEventListener("load", function() {
 
-    const loremIpsum = document.getElementById("lorem-ipsum");
-    fetch("https://baconipsum.com/api/?type=all-meat&paras=200&format=html")
-        .then(response => response.text())
-        // .then(result => loremIpsum.innerHTML = result)
+    // const loremIpsum = document.getElementById("lorem-ipsum");
+    // fetch("https://baconipsum.com/api/?type=all-meat&paras=200&format=html")
+    //     .then(response => response.text())
+    //     // .then(result => loremIpsum.innerHTML = result)
     /* 키는 이벤트 */
     const modal = document.getElementById("modal")
     const btnModal = document.getElementById("btn-modal")
